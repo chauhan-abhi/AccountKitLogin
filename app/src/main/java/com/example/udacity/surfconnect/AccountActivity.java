ceb56ee64712b71c41e45a82736eacbae5a26e29
chauhan-abhi <chahuhanabhi1245@gmail.com>
2017-07-20 12:13:49 +0530

    get account info

--- app/src/main/java/com/example/udacity/surfconnect/AccountActivity.java
+++ app/src/main/java/com/example/udacity/surfconnect/AccountActivity.java
@@ -1,9 +1,19 @@
 package com.example.udacity.surfconnect;
 
+import android.accessibilityservice.GestureDescription;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.facebook.accountkit.Account;
+import com.facebook.accountkit.AccountKit;
+import com.facebook.accountkit.AccountKitCallback;
+import com.facebook.accountkit.AccountKitError;
+import com.facebook.accountkit.PhoneNumber;
 import com.google.i18n.phonenumbers.NumberParseException;
 import com.google.i18n.phonenumbers.PhoneNumberUtil;
 import com.google.i18n.phonenumbers.Phonenumber;
@@ -26,6 +36,39 @@ public class AccountActivity extends AppCompatActivity {
         infoLabel = (TextView) findViewById(R.id.info_label);
         info = (TextView) findViewById(R.id.info);
 
+        AccountKit.getCurrentAccount(new AccountKitCallback<Account>() {
+            @Override
+            public void onSuccess(Account account) {
+                //Get Account Kit Id
+                String accountKitId = account.getId();
+                id.setText(accountKitId);
+
+                PhoneNumber phoneNumber = account.getPhoneNumber();
+                if(phoneNumber!=null){
+                    //If the phone number is available , display it
+                    String formattedPhoneNumber = formatPhoneNumber(phoneNumber.toString());
+                    info.setText(formattedPhoneNumber);
+                    infoLabel.setText(R.string.phone_label);
+                }else
+                {
+                    //If the email is available, display it
+                    String emailString = account.getEmail();
+                    info.setText(emailString);
+                    infoLabel.setText(R.string.email_label);
+                }
+            }
+
+            @Override
+            public void onError(AccountKitError accountKitError) {
+                String toastmessage =accountKitError.getErrorType().getMessage();
+                Toast.makeText(AccountActivity.this, toastmessage, Toast.LENGTH_LONG).show();
+                Log.i("error","failed to read account details");
+            }
+        });
+    }
+    public void onLogout(View view){
+        AccountKit.logOut();
+        launchLoginActivity();
     }
 
     private void launchLoginActivity() {
